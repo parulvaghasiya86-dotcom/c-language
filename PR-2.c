99ec9952b7053e19c0ff507380e2512c7dc3c35b
Shyam vaghasiya <parulvaghasiya86@gmail.com>
2025-11-18 16:55:56 +0530

    Add files via upload

---
+++ PR-2.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int main()
+{
+	int marks;
+	char grade;
+	printf("Enter your marks:");
+	scanf("%d" , &marks);
+ 	grade = (marks > 90) ?  'A' : 
+			(marks > 75) ? 'B' :
+			(marks > 60) ? 'C' :
+			(marks > 35) ? 'D' : 'F';
+		
+	switch (grade){
+		case 'A':
+			printf("Your grade is A\nExcellent work");
+			break;
+		case 'B':
+			printf("Your grade is B\nWell done");
+			break;
+		case 'C':
+			printf("Your grade is C\nGood job");
+			break;	
+		case 'D':
+			printf("Your grade is D\nYou passed , you could do better");
+			break;
+		default:
+			printf("Your grade is F\nSorry , you failed");
+			break;									
+	}		
+}
